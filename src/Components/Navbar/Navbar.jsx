a83bc35d426b8ee025adc775cc51493ce7237b4b
Ling7927 <jolinding27@gmail.com>
2023-06-01 19:46:49 +0800

    navbar:ok!

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { Grid } from "@mui/material";
+import { Grid, Menu, MenuItem, Box, Popper, Paper } from "@mui/material";
 import "./Nav.css";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import Logo from "../../assets/Logo.png";
@@ -8,7 +8,8 @@ import { routerList } from "../RouterList";
 const Navbar = () => {
   const [collapse, setCollapse] = useState("nav__menu");
   const [toggleIcon, setToggleIcon] = useState("toggler__icon");
-  const [onhandle,setOnHandle] =useState()
+  const [OnHover, setOnHover] = useState("");
+
   const onToggle = () => {
     collapse === "nav__menu"
       ? setCollapse("nav__menu nav__collapse")
@@ -17,42 +18,78 @@ const Navbar = () => {
     toggleIcon === "toggler__icon"
       ? setToggleIcon("toggler__icon toggle")
       : setToggleIcon("toggler__icon");
+
+    // lilist==="nav_li"
+    // ? "nav_lihover"
+    // :"nav_li";
+  };
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+    setOnHover("");
   };
 
   return (
- 
-        <Grid container spacing={1}>
-          <nav className="nav">
-            <Grid item xs={12} sm={12} md={3}>
-
-              <Link to="/" style={{ borderBottom: "none" }}>
-                <img
-                  src={Logo}
-                  alt="Logo"
-                  style={{ width: "10rem", marginLeft: " 1.5rem" }}
-                />
-              </Link>
-            </Grid>
-            <Grid item xs={12} sm={12} md={9}>    
+    <>
+      <Grid container spacing={1}>
+        <nav className="nav">
+          <Grid item xs={12} sm={12} md={3}>
+            <Link to="/" style={{ borderBottom: "none" }}>
+              <img
+                src={Logo}
+                alt="Logo"
+                style={{ width: "10rem", marginLeft: " 1.5rem" }}
+              />
+            </Link>
+          </Grid>
+          <Grid item xs={12} sm={12} md={9}>
             <ul className={collapse}>
-              {routerList.map((item) => (
-                <li key={item.name} className="nav__item">
-                  <NavLink to={item.path} className="nav__link">
-                    {item.name}
-                  </NavLink>
-                </li>
-              ))}
+              {routerList.map((item) => {
+                if (item.icon === null) {
+                  return (
+                    <li
+                      key={item.name}
+                      className="nav__item"
+                      onMouseEnter={handleClose}
+                    >
+                      <NavLink to={item.path} className="nav__link">
+                        {item.name}
+                      </NavLink>
+                    </li>
+                  );
+                } else {
+                  return (
+                    <div className="dropdown" style={{ float: "left" }}>
+                      <button className="dropbtn">
+                        {item.name} {item.icon}
+                      </button>
+                      <div className="dropdown-content" style={{ left: "0" }}>
+                        {item.li.map((li, index) => {
+                          return (
+                            <NavLink to={li.lipath} key={index}>
+                              {li.liname}
+                            </NavLink>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  );
+                }
+              })}
             </ul>
             <div className={toggleIcon} onClick={onToggle}>
-             <div>
-               <AiOutlineMenu />
-             </div>
-               
-              
+              <div>
+                <AiOutlineMenu />
+              </div>
             </div>
-            </Grid>
-          </nav>
-        </Grid>
+          </Grid>
+        </nav>
+      </Grid>
+    </>
     //   </div>
     // </div>
   );
--- src/Components/RouterList.js
+++ src/Components/RouterList.js
@@ -1,5 +1,5 @@
-import News from "../Pages/News/News"
-import Introduction from "../Pages/Introduction/Introduction"
+import News from "../Pages/News/News";
+import Introduction from "../Pages/Introduction/Introduction";
 import Specification from "../Pages/Specification/Specification";
 import Traks from "../Pages/Traks/Traks";
 import Result from "../Pages/Result/Result";
@@ -7,71 +7,107 @@ import Successcase from "../Pages/Successcase/Successcase";
 
 import Member from "../Pages/Member/Member";
 import Recruit from "../Pages/Recruit/Recruit";
-import  {AiFillCaretDown } from "react-icons/ai";
+import { AiFillCaretDown } from "react-icons/ai";
 const routerList = [
-    {
-      name: "最新消息",
-      path: "/News",
-      Component: <News />,
-    },
-    {
-        name: "簡介",
-        path: "/Introduction",
-        Component: <Introduction />,
-      },
-      // {
-        //     name: "招募",
-        //     path: "/Information",
-        //     Component: <Information />,
-        //   },
-        {
-          name: <>招募 <AiFillCaretDown/></>,
-          path: "",
-          Component: "",
-        },
-      {
-        name: "規格與資源",
-        path: "/Specification",
-        Component: <Specification />,
-      },      
-      {
-          name: "賽道",
-          path: "/Traks",
-          Component: <Traks />,
-        },
-        
-        {
-          name: "2022聯測松結果",
-          path: "/Result",
-          Component: <Result />,
-        },
-        {
-          name: "成果發表",
-          path: "/Successcase",
-          Component: <Successcase />,
-        },
-        {
-          name: "歷年活動",
-          path: "",
-          Component: "",
-        },
-        {
-          name: "活動",
-          path: "",
-          Component: "",
-        },
- ];
-
- const RecruitDropDown = [
   {
-    name: "賽道工作小組",
-    path: "/Member",
-    Component: <Member />,
+    name: "最新消息",
+    icon: null,
+    path: "/News",
+    Component: <News />,
   },
   {
-    name: "工作小組志工與聯測督察員招募",
-    path: "/Recruit",
-    Component: <Recruit />,
+    name: "簡介",
+    icon: null,
+    path: "/Introduction",
+    Component: <Introduction />,
+  },
+  // {
+  //     name: "招募",
+  //     path: "/Information",
+  //     Component: <Information />,
+  //   },
+  {
+    name: "招募",
+    icon: <AiFillCaretDown />,
+    path: "",
+    Component: "",
+    li: [
+      { liname: "賽道工作小組",
+        lipath: "/Member",
+        liComponent: <Member /> },
+      {
+        liname: "工作小組志工與聯測督察員招募",
+        lipath: "/Recruit",
+        liComponent: <Recruit />,
+      },
+    ],
+  },
+  {
+    name: "規格與資源",
+    icon: null,
+    path: "/Specification",
+    Component: <Specification />,
+  },
+  {
+    name: "賽道",
+    icon: null,
+    path: "/Traks",
+    Component: <Traks />,
+  },
+
+  {
+    name: "2022聯測松結果",
+    icon: null,
+    path: "/Result",
+    Component: <Result />,
+  },
+  {
+    name: "成果發表",
+    icon: null,
+    path: "/Successcase",
+    Component: <Successcase />,
+  },
+  {
+    name: "歷年活動",
+    icon: <AiFillCaretDown />,
+    path: "",
+    Component: "",
+    li: [
+      { liname: "2022年 MI-TW 聯測工作坊",
+        lipath: "/",
+        liComponent: ""},
+      {
+        liname: "2021年 MI-TW 聯測工作坊",
+        lipath: "/",
+        liComponent: "",
+      },
+      {
+        liname: "2020年 MI-TW 聯測工作坊",
+        lipath: "/",
+        liComponent: "",
+      },
+    ],
+  },
+  {
+    name: "活動",
+    icon: <AiFillCaretDown />,
+    path: "",
+    Component: "",
+    li: [
+      { liname: "工作小組",
+        lipath: "/",
+        liComponent: "" },
+      {
+        liname: "相關活動",
+        lipath: "/",
+        liComponent: "",
+      },
+      {
+        liname: "教育訓練",
+        lipath: "/",
+        liComponent: "",
+      },
+    ],
   },
 ];
 
@@ -111,4 +147,4 @@ const routerList = [
 //   },
 // ];
 
-export {routerList,RecruitDropDown}
+export { routerList };

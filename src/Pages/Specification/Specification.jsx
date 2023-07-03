70b705dc9f4ab6f8830b942fab63fd26c5c97e00
Ling7927 <jolinding27@gmail.com>
2023-07-03 23:58:37 +0800

    Specification Grid

--- src/Pages/Specification/Specification.jsx
+++ src/Pages/Specification/Specification.jsx
@@ -1,5 +1,6 @@
 import { React, useState } from "react";
 import { P1, ListTitle, List } from "./SpecificationInform";
+import { Grid } from "@mui/material";
 import { motion } from "framer-motion";
 function Specification() {
   const [onHandle, setOnHandle] = useState("1");
@@ -8,9 +9,9 @@ function Specification() {
       <div style={{ background: "#d9d9d9" }}>
         <div
           style={{
-            margin: "0 5vw 1vw",
+            margin: "0 5vw ",
             background: "#fdfdfd",
-            paddingTop: "2vw",
+            padding: "2vw 0",
           }}
         >
           {/* 公開意見徵求 */}
@@ -27,7 +28,7 @@ function Specification() {
               justifyContent: "center",
               alignItems: "center",
               padding: "2vw",
-              margin: "2vw 5vw",
+              margin: "2vw 5vw 0",
             }}
           >
             <h4 style={{ marginBottom: "2rem" }}> {P1.title}</h4>
@@ -39,73 +40,82 @@ function Specification() {
           </motion.div>
 
           {/* 說明 */}
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              margin: "0 5vw",
-            }}
-          >
-            {/* 選單 */}
-            <motion.div
-              initial={{ x: -200 }}
-              animate={{ x: 0 }}
-              transition={{ duration: 0.8 }}
-              style={{ width: "20%" }}
+
+          <Grid container style={{}}>
+            <Grid
+              item
+              sm={12}
+              md={12}
+              lg={3}
+              style={{ display: "flex", justifyContent: "center" }}
             >
-              {ListTitle.map((item) => {
-                return (
-                  <div>
-                    <button
-                      onClick={() => setOnHandle(item.id)}
-                      key={item}
-                      style={{
-                        margin: 0,
-                        backgroundColor: "Transparent",
-                        backgroundRepeat: "no-repeat",
-                        whiteSpace: "nowrap", //不換行
-                        border: "none",
-                        //cursor: "pointer",//鼠標形狀改變
-                        overflow: "hidden",
-                        fontSize: onHandle === item.id ? "15px" : "5px",
-                        color: onHandle === item.id ? "#fff" : "#949494",
-                      }}
-                    >
-                      {item.title}
-                    </button>
-                  </div>
-                );
-              })}
-            </motion.div>
+              {/* 選單 */}
+              <motion.div
+                initial={{ x: -200 }}
+                animate={{ x: 0 }}
+                transition={{ duration: 0.8 }}
+                style={{ width: "20%" }}
+              >
+                {ListTitle.map((item) => {
+                  return (
+                    <div>
+                      <button
+                        onClick={() => setOnHandle(item.id)}
+                        key={item}
+                        style={{
+                          margin: 0,
+                          backgroundColor: "Transparent",
+                          backgroundRepeat: "no-repeat",
+                          whiteSpace: "nowrap", //不換行
+                          border: "none",
+                          //cursor: "pointer",//鼠標形狀改變
+                          overflow: "hidden",
+                          fontSize: onHandle == item.id ? "15px" : "5px",
+                          color: onHandle === item.id ? "#000" : "#949494",
+                        }}
+                      >
+                        {item.title}
+                      </button>
+                    </div>
+                  );
+                })}
+              </motion.div>
+            </Grid>
             {/* 說明內容 */}
-            <div
-              style={{ width: "75%", display: "flex", flexDirection: "column" }}
-            >
-              <motion.h5
-                initial={{ opacity: 0, scale: 0.8 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 1.2 }}
+            <Grid item sm={12} md={12} lg={9}>
+              <div
                 style={{
-                  margin: "0.5rem 0",
-                  padding: "0 1rem ",
-                  borderLeft: "2px solid #F5CE85",
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: "0 5vw 2vh",
                 }}
               >
-                {List.filter((item) => item.id === onHandle)[0].title}
-              </motion.h5>
-              <motion.p
-                initial={{ opacity: 0, scale: 0.8 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 1 }}
-                style={{ fontSize: "1vw" }}
-              >
-                {List.filter((item) => item.id === onHandle)[0].content}
-              </motion.p>
+                <motion.h5
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 1.2 }}
+                  style={{
+                    margin: "0.5rem 0",
+                    padding: "0 1rem ",
+                    borderLeft: "2px solid #F5CE85",
+                  }}
+                >
+                  {List.filter((item) => item.id === onHandle)[0].title}
+                </motion.h5>
+                <motion.p
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{ duration: 1 }}
+                  style={{ fontSize: "1vw" }}
+                >
+                  {List.filter((item) => item.id === onHandle)[0].content}
+                </motion.p>
 
-              {/* <h4>{item.title}</h4>
+                {/* <h4>{item.title}</h4>
               {item.content} */}
-            </div>
-          </div>
+              </div>
+            </Grid>
+          </Grid>
         </div>
       </div>
     </>

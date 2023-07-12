ca4d5e164edc1a9a2ae93db581af4143747c8079
Ling7927 <jolinding27@gmail.com>
2023-07-12 13:04:09 +0800

    Member

--- src/Pages/Member/Member.jsx
+++ src/Pages/Member/Member.jsx
@@ -6,24 +6,7 @@ import { Grid } from "@mui/material";
 function Member() {
   return (
     <>
-      {/* // <div style={{ margin: "3rem" }}>
-        // <MemberSlider />
-        //  <MemberCard /> 
-       // </div>  */}
       <Grid container spacing={1} style={{ padding: "2rem  2rem 3rem" }}>
-        {/* <Grid item sm={12} md={12} lg={12}>
-          <h4>賽道工作小組</h4>
-          <hr />
-        </Grid>
-
-        <Grid item sm={1} md={1} lg={1}>
-          <TrackTab />
-        </Grid>
-        <Grid item sm={10} md={10} lg={10}>
-          <MemberSlider />
-        </Grid> */}
-        {/* ------------------------ */}
-        {/* <TrackTab /> */}
         <MemberCard />
       </Grid>
     </>

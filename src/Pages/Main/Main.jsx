8b54364f86ddedb5d25d36d0768cfc75e0fc4a06
Ling7927 <jolinding27@gmail.com>
2023-07-18 17:26:46 +0800

    Update Main

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -4,11 +4,11 @@ import { AiOutlineDownload } from "react-icons/ai";
 // import T2023 from "../../assets/T2023.png";
 // import Ttext from "../../assets/Titletext.png";
 import Main1 from "../../assets/mainbackground.jpg";
-import maintitle from "../../assets/Title.png"
+import maintitle from "../../assets/Title.png";
 import { useRef } from "react";
 import { useInView } from "framer-motion";
-import { motion} from "framer-motion";
-import "./Main.css"
+import { motion } from "framer-motion";
+import "./Main.css";
 function Main() {
   const ref = useRef(null);
   const isInView = useInView(ref);
@@ -52,46 +52,45 @@ function Main() {
             ></motion.img>
           </div> */}
       <div
-      className="Mainbackground"
+        className="Mainbackground"
         style={{
           //backgroundSize: window.innerWidth,
           backgroundImage: `url(${Main1})`,
-         
         }}
       >
-         <motion.img
-              ref={ref}
-              src={maintitle}
-              className="Maintitle"
-              style={{
-                
-                transform: isInView
-                  ? "translateX(0)"
-                  : "translateX(-200px)",
-                opacity: isInView ? 1 : 0,
-                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-              }}
-            ></motion.img>
+        <motion.img
+          ref={ref}
+          src={maintitle}
+          className="Maintitle"
+          style={{
+            transform: isInView ? "translateX(0)" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+        ></motion.img>
+        {/* -------------------DM下載--------------------- */}
+
+        <motion.div
+          style={{
+            display: "flex",
+            position: "absolute",
+            bottom: "3rem",
+            textAlign: "center",
+            justifyContent: "center",
+            margin: "3rem 3rem",
+            fontSize: "25px",
+          }}
+        >
+          <a
+            href="https://mitw.dicom.org.tw/pdf/mitw2023_DM.pdf"
+            target="_blank"
+            style={{ color: "#fff" }}
+          >
+            MITW-DM 宣傳單下載 <AiOutlineDownload />
+          </a>
+        </motion.div>
       </div>
       {/* </div> */}
-
-      {/* -------------------DM下載--------------------- */}
-      <div
-        style={{
-          display: "flex",
-          textAlign: "center",
-          justifyContent: "center",
-          margin: "3rem 3rem",
-          fontSize: "25px",
-        }}
-      >
-        <a     href="https://mitw.dicom.org.tw/pdf/mitw2023_DM.pdf"
-          target="_blank"
-          style={{ color: "#000" }}
-        >
-          MITW-DM 宣傳單下載 <AiOutlineDownload />
-        </a>
-      </div>
     </>
   );
 }

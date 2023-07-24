d80761dd9b4c5f1e6f5b2b500a5e9813c410d606
Ling7927 <jolinding27@gmail.com>
2023-07-24 13:09:09 +0800

    Update card RWD

--- src/Pages/Member/Member.jsx
+++ src/Pages/Member/Member.jsx
@@ -1,21 +1,10 @@
 import React from "react";
 import MemberCard from "../../Components/MemberCard/MemberCard";
-import MemberSlider from "../../Components/MemberCard/MemberSlider";
-import TrackTab from "../../Components/MemberCard/TrackTab";
-import { Grid } from "@mui/material";
+import "./Member.css";
 function Member() {
   return (
     <>
-      <div
-        style={{
-          padding: "0  5vh 3vh",
-          minHeight: "100vh",
-          background:
-            "linear-gradient( 180deg, #ffffff 0%, #6aadd9 0.01%, rgba(0, 249, 255, 0) 56.56%, rgba(240, 240, 240, 0) 100%",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
+      <div className="Membackground">
         <MemberCard />
       </div>
     </>

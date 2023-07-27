7cfc34d567c37e8ef8638bff232b6ed8cff547cd
Ling7927 <jolinding27@gmail.com>
2023-07-27 11:23:11 +0800

    End V1.1

--- src/Pages/Relatedactivity/Relatedactivity.jsx
+++ src/Pages/Relatedactivity/Relatedactivity.jsx
@@ -10,48 +10,51 @@ function Relatedactivity() {
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
+          alignItems: "center",
           background:
             "linear-gradient(180deg, #000000 0%, #6AADD9 0.01%, rgba(0, 249, 255, 0) 56.56%, rgba(240, 240, 240, 0) 100%)",
+          padding: "0 5vh 5vh",
         }}
       >
         <div
           style={{
+            background: "#fdfdfd",
             display: "flex",
             flexDirection: "column",
-            justifyContent: "center",
             alignItems: "center",
-            margin: "2vw 0 0",
           }}
         >
-          <h3
+          <div
             style={{
-              borderWidth: "2px",
-              paddingTop: "1vw",
-              fontWeight: "bold",
-              marginBottom: "2rem",
-              color: "#fdfdfd",
-              padding: " 1rem 5rem 1.5rem",
-              borderBottom: "solid 4px #fbc170",
-              // borderStyle: "solid",
-
-              // boxShadow: " rgb(118, 189, 213) 2px 1px 3px 2px",
-
-              // borderImage:
-              //   "linear-gradient(rgb(35, 240, 199), rgb(16, 137, 152), rgb(16, 249, 255)) 20 / 1 / 0 stretch",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "2vw 0 0",
             }}
           >
-            相關活動
-          </h3>
-        </div>
-
-        {active.map((item) => {
-          return (
-            <div
+            <h3
               style={{
-                display: "flex",
-                justifyContent: "center",
+                borderWidth: "2px",
+                paddingTop: "1vw",
+                fontWeight: "bold",
+                marginBottom: "2rem",
+                padding: " 1rem 5rem 1.5rem",
+                borderBottom: "solid 4px #fbc170",
+                // borderStyle: "solid",
+
+                // boxShadow: " rgb(118, 189, 213) 2px 1px 3px 2px",
+
+                // borderImage:
+                //   "linear-gradient(rgb(35, 240, 199), rgb(16, 137, 152), rgb(16, 249, 255)) 20 / 1 / 0 stretch",
               }}
             >
+              相關活動
+            </h3>
+          </div>
+
+          {active.map((item) => {
+            return (
               <div key={item} className="ActiveBlock">
                 <h3
                   style={{
@@ -68,9 +71,9 @@ function Relatedactivity() {
                   {item.content}
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
       {/* </div> */}
     </>
--- src/Pages/Workgroupactivity/Workgroupactivity.jsx
+++ src/Pages/Workgroupactivity/Workgroupactivity.jsx
@@ -11,45 +11,51 @@ function Workgroupactivity() {
         // background: "rgb(253, 253, 253)",
         background:
           "linear-gradient(180deg, #000000 0%, #6AADD9 0.01%, rgba(0, 249, 255, 0) 56.56%, rgba(240, 240, 240, 0) 100%)",
-
-        minHeight: "100%",
+        padding: "0 5vh 5vh",
       }}
     >
       <div
         style={{
+          background: "#fdfdfd",
           display: "flex",
           flexDirection: "column",
-          justifyContent: "center",
           alignItems: "center",
-          margin: "2vw 0 ",
         }}
       >
-        <h3
+        <div
           style={{
-            // borderWidth: "2px",
-            // padding: "1vw",
-            // fontWeight: "bold",
-            // margin: 0,
-            // =================
-            // borderStyle: "solid",
-
-            // boxShadow: " rgb(118, 189, 213) 2px 1px 3px 2px",
-
-            // borderImage:
-            //   "linear-gradient(rgb(35, 240, 199), rgb(16, 137, 152), rgb(16, 249, 255)) 20 / 1 / 0 stretch",
-            // ======================
-            borderWidth: "2px",
-            paddingTop: "1vw",
-            fontWeight: "bold",
-            marginBottom: "2rem",
-            color: "#fdfdfd",
-            padding: " 1rem 5rem 1.5rem",
-            borderBottom: "solid 4px #fbc170",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "2vw 0 ",
           }}
         >
-          工作小組
-        </h3>
-        {/* <hr
+          <h3
+            style={{
+              // borderWidth: "2px",
+              // padding: "1vw",
+              // fontWeight: "bold",
+              // margin: 0,
+              // =================
+              // borderStyle: "solid",
+
+              // boxShadow: " rgb(118, 189, 213) 2px 1px 3px 2px",
+
+              // borderImage:
+              //   "linear-gradient(rgb(35, 240, 199), rgb(16, 137, 152), rgb(16, 249, 255)) 20 / 1 / 0 stretch",
+              // ======================
+              borderWidth: "2px",
+              paddingTop: "1vw",
+              fontWeight: "bold",
+              marginBottom: "2rem",
+              padding: " 1rem 5rem 1.5rem",
+              borderBottom: "solid 4px #fbc170",
+            }}
+          >
+            工作小組
+          </h3>
+          {/* <hr
           style={{
             width: "20%",
             marginBottom: "2rem",
@@ -58,41 +64,42 @@ function Workgroupactivity() {
             border: "2px solid orange",
           }}
         /> */}
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        {active.map((item) => {
-          return (
-            <div key={item} className="ActiveBlock">
-              <h3
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  fontWeight: "bold",
-                  marginBottom: "2vw",
-                }}
-              >
-                {" "}
-                {item.title}
-              </h3>
-              <div
-                style={{
-                  display: "flex",
-                  width: "100%",
-                  whiteSpace: "normal",
-                }}
-              >
-                {item.content}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {active.map((item) => {
+            return (
+              <div key={item} className="ActiveBlock">
+                <h3
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    fontWeight: "bold",
+                    marginBottom: "2vw",
+                  }}
+                >
+                  {" "}
+                  {item.title}
+                </h3>
+                <div
+                  style={{
+                    display: "flex",
+                    width: "100%",
+                    whiteSpace: "normal",
+                  }}
+                >
+                  {item.content}
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
